d0922ce77cc327f55bd0deff120f000883e56d46
Patricio Castro <luckydrawings@gmail.com>
2020-08-06 18:25:17 +0200

    deploy 34567890

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,18 +8,26 @@ let navBtn = document.querySelector(".nav-button");
 let itemsNav = document.querySelector(".nav-menu a");
 let burgerLines = document.querySelector(".lines");
 
-zodiacBtn.addEventListener("click", () => {
-  zbuttons.classList.toggle("show-list")
-  abuttons.classList.remove("show-list")
-})
-
-alphaBtn.addEventListener("click", () => {
-  abuttons.classList.toggle("show-list")
-  zbuttons.classList.remove("show-list")
-})
-
 navBtn.addEventListener("click", () => {
     navBtn.classList.toggle("show-menu");
     burgerLines.classList.toggle("close");
     navMenu.classList.toggle("show-menu");
 });
+
+if (zodiacBtn) {
+    zodiacBtn.addEventListener("click", () => {
+        zbuttons.classList.toggle("show-list")
+        abuttons.classList.remove("show-list")
+    })
+}
+if (alphaBtn) {
+    alphaBtn.addEventListener("click", () => {
+        abuttons.classList.toggle("show-list")
+        zbuttons.classList.remove("show-list")
+    })
+}
+
+
+
+
+
